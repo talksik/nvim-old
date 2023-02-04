20b54437ff6f21fbb4858b9fdf26510673ea98b6
Arjun Patel <patel.arjun50@gmail.com>
2023-02-03 20:57:47 -0600

    search diagnostic with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,4 +5,5 @@ vim.keymap.set('n', '<leader>sw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>st', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
 

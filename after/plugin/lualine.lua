09aac154e1cbbacf5fa9601a467cdd0a3b66c811
Arjun Patel <patel.arjun50@gmail.com>
2023-02-07 10:40:05 -0600

    better adding in rel path for lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,5 +2,12 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'codedark',
+    },
+    sections = {
+        lualine_a = {
+            { 'filename',
+                path = 1,
+            }
+        }
     }
 }

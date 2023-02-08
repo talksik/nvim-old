06cb7862abaae2b3fc4509263178e2329083c592
Arjun Patel <patel.arjun50@gmail.com>
2023-02-08 12:39:36 -0600

    getting rid of previewer and adding in fuzzy finder

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,18 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup {
+    defaults = {
+        preview = false,
+    },
+}
+
+-- disable preview in telescope
+vim.g.telescope_previewer_enable = false
+
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ww', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>st', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
-
+vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})

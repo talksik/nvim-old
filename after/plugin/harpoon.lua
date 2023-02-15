73e953a6f6950009061c626d7648e300d9438564
Arjun Patel <patel.arjun50@gmail.com>
2023-02-15 06:21:12 -0600

    per git branch harpoon list

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,10 @@
+local harpoon = require("harpoon")
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+
+harpoon.setup({
+  mark_branch = true,
+})
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
@@ -8,5 +13,3 @@ vim.keymap.set("n", "<C-g>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-b>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
-
-
